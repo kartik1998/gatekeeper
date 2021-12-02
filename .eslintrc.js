c0696789dd89f95459f036f424eef39e2f5eb3f4
kartik1998 <kartikrawat61@gmail.com>
2021-12-02 19:48:52 +0530

    refactor(update): eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,30 @@
+const path = require('path');
+const pkg = require('./package.json');
+
+const aliases = Object.entries(pkg._moduleAliases || {}).map((e) => [e[0], path.resolve(__dirname, e[1])]);
+
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true
-  },
-  extends: [
-    'standard'
-  ],
-  parserOptions: {
-    ecmaVersion: 12
-  },
+  extends: 'airbnb-base',
   rules: {
-  }
-}
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: ['_moduleAliases'],
+      },
+    ],
+    'import/no-extraneous-dependencies': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-return-assign': ['error', 'except-parens'],
+    'no-cond-assign': ['error', 'except-parens'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'max-len': ['off'],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: aliases,
+        extensions: ['.js', 'json'],
+      },
+    },
+  },
+};
