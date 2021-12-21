f1f678752618163555bd13e00d148236962d4fc1
kartik1998 <kartikrawat61@gmail.com>
2021-12-22 02:43:30 +0530

    chore: add sample express app

--- _sample/index.js
+++ _sample/index.js
@@ -1,10 +1,27 @@
-const { AppModule } = require('../index');
+// const { AppModule } = require('../index');
 
-const web = new AppModule({ logWebHookToConsole: false });
+// const web = new AppModule({ logWebHookToConsole: false });
 
-(async function () {
-  const res = await web.waitForWebHook();
-  console.log({ res });
-  const res1 = await web.waitForWebHook();
-  console.log({ res1 });
-})();
+// (async function () {
+//   const res = await web.waitForWebHook();
+//   console.log({ res });
+//   const res1 = await web.waitForWebHook();
+//   console.log({ res1 });
+// })();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/time/:time', async function (req, res) {
+  const { time } = req.params;
+  setTimeout(function () {
+    res.json({ msg: `response in ${time} seconds` });
+  }, time * 1000);
+});
+
+app.get('/ping', async function (req, res) {
+  return res.send('pong');
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
