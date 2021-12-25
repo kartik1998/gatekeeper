26db3bfb896df69c1dd2b8e6385ac8a936fa8f65
kartik1998 <kartikrawat61@gmail.com>
2021-12-25 18:33:30 +0530

    refactor: _sample

--- _sample/request.js
+++ _sample/request.js
@@ -3,4 +3,4 @@ const axios = require('axios').default;
 (async function () {
   const result = await axios.get('http://localhost:5000/time/1');
   console.log({ data: result.data });
-}());
+})();
--- _sample/test.js
+++ _sample/test.js
@@ -2,10 +2,12 @@ const { AppModule } = require('../index');
 const app = require('./index');
 
 const web = new AppModule({ logWebHookToConsole: false, app });
+console.log(web.getLocalWebhookUrl());
+console.log(web.getNgrokWebhookUrl());
 
 (async function () {
   const res1 = await web.waitForWebHook();
   console.log({ res1 });
   const res2 = await web.waitForWebHook();
   console.log({ res2 });
-}());
+})();
