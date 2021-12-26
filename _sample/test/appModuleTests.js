310ad6d81299298da13690e24b8e94d6e4bd2d62
kartik1998 <kartikrawat61@gmail.com>
2021-12-26 20:27:20 +0530

    test: add seperate test for 2 seconds delay

--- _sample/test/appModuleTests.js
+++ _sample/test/appModuleTests.js
@@ -4,7 +4,6 @@ const { AppModule } = require('../../index');
 const app = require('../index');
 
 const web = new AppModule({ logWebHookToConsole: false, app });
-console.log(web.getLocalWebhookUrl());
 
 describe('AppModule _sample tests', function () {
   before(async () => {
@@ -25,6 +24,22 @@ describe('AppModule _sample tests', function () {
     } else {
       expect(webhookResponse.method).to.equal('POST');
       expect(webhookResponse.body.wait).to.equal('1.002');
+    }
+  });
+
+  it('returns response in time in seconds specified in params', async () => {
+    const response = await request(app).get('/time/2');
+    expect(response.status).to.equal(200);
+    const id = web.getGatekeeperId();
+    const webhookResponse = await web.waitForWebHook();
+    if (id !== webhookResponse.headers['x-gatekeeper-id']) {
+      // it depends on user on how to handle this case. This probably means that a webhook of
+      // a previous test has been recieved now. you can either throw an error in this case or just continue;
+      // You can also handle this by just writing an assertion like:
+      // expect(webhookResponse.headers['x-gatekeeper-id']).to.equal(id);
+    } else {
+      expect(webhookResponse.method).to.equal('POST');
+      expect(webhookResponse.body.wait).to.equal('2.004');
     }
   });
 });
