b17ab91ee70e19ec2e5ec6e5dab77b44ad44a323
kartik1998 <kartikrawat61@gmail.com>
2022-01-22 21:26:46 +0530

    refactor: add comments in sample test and remove unwanted scripts

--- _sample/test/appModuleTests.js
+++ _sample/test/appModuleTests.js
@@ -3,25 +3,30 @@ const { expect } = require('chai');
 const { AppModule } = require('../../build/src/index');
 const app = require('../index');
 
-const instance = AppModule.Instance({ debug: true });
+const webhook = AppModule.Instance({ debug: true });
 
 describe('AppModule _sample tests', function () {
   this.timeout(100000);
   before(async () => {
-    await instance.startWebhookServer();
-    const ngrokUrl = await instance.getNgrokUrl();
-    const localUrl = await instance.getLocalUrl();
+    await webhook.startWebhookServer();
+    const ngrokUrl = await webhook.getNgrokUrl();
+    const localUrl = await webhook.getLocalUrl();
     process.env.WEBHOOK_URL = localUrl;
   });
+
   it('returns response in time in seconds specified in params', async () => {
-    const webhookTestId = instance.createWebhookTestId();
+    const webhookTestId = webhook.createWebhookTestId(); // create webhook test id
+    // synchronous api response assertions
     const response = await request(app).get('/time/1');
     expect(response.status).to.equal(200);
-    const webhookResponse = await instance.wait();
+    // synchronous api response assertions
+
+    // assertions for webhooks recieved
+    const webhookResponse = await webhook.wait();
     expect(webhookResponse.body.msg).to.equal('webhook triggered immediately');
     expect(webhookResponse.body.wait).to.equal('~0');
     expect(webhookResponse.headers['x-webhooktest-id']).to.equal(webhookTestId);
-    const webhookResponse2 = await instance.wait();
+    const webhookResponse2 = await webhook.wait();
     expect(webhookResponse2.body.msg).to.equal('webhook response in 1 + dt seconds');
     expect(webhookResponse2.body.wait).to.equal('1.002');
     expect(webhookResponse2.headers['x-webhooktest-id']).to.equal(webhookTestId);
