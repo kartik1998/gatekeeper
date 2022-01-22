cd3d8f113d96a48e60109879366769e635a148ea
kartik1998 <kartikrawat61@gmail.com>
2022-01-22 16:40:09 +0530

    refactor: add test to compare webhookTestId

--- _sample/test/appModuleTests.js
+++ _sample/test/appModuleTests.js
@@ -3,7 +3,8 @@ const { expect } = require('chai');
 const { AppModule } = require('../../build/src/index');
 const app = require('../index');
 
-const instance = AppModule.Instance({ debug: true });
+const instance = AppModule.Instance({ debug: true }, app);
+instance.enableWebhookHeaderModification();
 
 describe('AppModule _sample tests', function () {
   this.timeout(100000);
@@ -14,13 +15,16 @@ describe('AppModule _sample tests', function () {
     process.env.WEBHOOK_URL = ngrokUrl;
   });
   it('returns response in time in seconds specified in params', async () => {
+    instance.createWebhookTestId();
     const response = await request(app).get('/time/1');
     expect(response.status).to.equal(200);
     const webhookResponse = await instance.wait();
     expect(webhookResponse.body.msg).to.equal('webhook triggered immediately');
     expect(webhookResponse.body.wait).to.equal('~0');
+    expect(webhookResponse.headers['x-webhooktest-id']).to.equal(instance.getWebhookTestId())
     const webhookResponse2 = await instance.wait();
     expect(webhookResponse2.body.msg).to.equal('webhook response in 1 + dt seconds');
     expect(webhookResponse2.body.wait).to.equal('1.002');
+    expect(webhookResponse2.headers['x-webhooktest-id']).to.equal(instance.getWebhookTestId())
   });
 });
