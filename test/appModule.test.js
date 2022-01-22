4221ed8aef3f014d761b64cac165276984a6a1a8
kartik1998 <kartikrawat61@gmail.com>
2022-01-22 22:19:46 +0530

    chore: setup tests

---
+++ test/appModule.test.js
@@ -0,0 +1,34 @@
+const request = require('supertest');
+const { expect } = require('chai');
+const { AppModule } = require('../build/src/index');
+const app = require('../_sample/index');
+
+const webhook = AppModule.Instance({ debug: true });
+
+describe('AppModule tests', function () {
+  this.timeout(100000);
+  before(async () => {
+    await webhook.startWebhookServer();
+    const ngrokUrl = await webhook.getNgrokUrl();
+    const localUrl = await webhook.getLocalUrl();
+    process.env.WEBHOOK_URL = localUrl;
+  });
+
+  it('returns response in time in seconds specified in params', async () => {
+    const webhookTestId = webhook.createWebhookTestId(); // create webhook test id
+    // synchronous api response assertions
+    const response = await request(app).get('/time/1');
+    expect(response.status).to.equal(200);
+    // synchronous api response assertions
+
+    // assertions for webhooks recieved
+    const webhookResponse = await webhook.wait();
+    expect(webhookResponse.body.msg).to.equal('webhook triggered immediately');
+    expect(webhookResponse.body.wait).to.equal('~0');
+    expect(webhookResponse.headers['x-webhooktest-id']).to.equal(webhookTestId);
+    const webhookResponse2 = await webhook.wait();
+    expect(webhookResponse2.body.msg).to.equal('webhook response in 1 + dt seconds');
+    expect(webhookResponse2.body.wait).to.equal('1.002');
+    expect(webhookResponse2.headers['x-webhooktest-id']).to.equal(webhookTestId);
+  });
+});
