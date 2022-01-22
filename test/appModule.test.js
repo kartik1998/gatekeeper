7bb23a6f3fb7909559af20881552165ab3f19680
kartik1998 <kartikrawat61@gmail.com>
2022-01-22 22:34:59 +0530

    chore: add test for 3 seconds

--- test/appModule.test.js
+++ test/appModule.test.js
@@ -14,7 +14,7 @@ describe('AppModule tests', function () {
     process.env.WEBHOOK_URL = localUrl;
   });
 
-  it('returns response in time in seconds specified in params', async () => {
+  it('returns response in time in seconds specified in params (1 second)', async () => {
     const webhookTestId = webhook.createWebhookTestId(); // create webhook test id
     // synchronous api response assertions
     const response = await request(app).get('/time/1');
@@ -29,6 +29,24 @@ describe('AppModule tests', function () {
     const webhookResponse2 = await webhook.wait();
     expect(webhookResponse2.body.msg).to.equal('webhook response in 1 + dt seconds');
     expect(webhookResponse2.body.wait).to.equal('1.002');
+    expect(webhookResponse2.headers['x-webhooktest-id']).to.equal(webhookTestId);
+  });
+
+  it('returns response in time in seconds specified in params (3 seconds)', async () => {
+    const webhookTestId = webhook.createWebhookTestId(); // create webhook test id
+    // synchronous api response assertions
+    const response = await request(app).get('/time/3');
+    expect(response.status).to.equal(200);
+    // synchronous api response assertions
+
+    // assertions for webhooks recieved
+    const webhookResponse = await webhook.wait();
+    expect(webhookResponse.body.msg).to.equal('webhook triggered immediately');
+    expect(webhookResponse.body.wait).to.equal('~0');
+    expect(webhookResponse.headers['x-webhooktest-id']).to.equal(webhookTestId);
+    const webhookResponse2 = await webhook.wait();
+    expect(webhookResponse2.body.msg).to.equal('webhook response in 3 + dt seconds');
+    expect(webhookResponse2.body.wait).to.equal('3.006');
     expect(webhookResponse2.headers['x-webhooktest-id']).to.equal(webhookTestId);
   });
 });
